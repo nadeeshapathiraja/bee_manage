7bf5211fef72b0073ac436733e19883039ef2050
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2024-10-21 00:15:52 +0530

    project add part

--- lib/pages/main_pages/home_page/home_page.dart
+++ lib/pages/main_pages/home_page/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:beetracker/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,10 +12,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.builder(
+            itemCount: 15,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: kWhite.withOpacity(0.9),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                        ),
+                        width: 100,
+                        height: 100,
+                        child: Image.asset(
+                          'assets/images/hive.png',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
